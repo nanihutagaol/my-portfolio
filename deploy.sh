0d2919be82e2e1caada0dc43bfdd868467d63191
Nani Renova Hutagaol <nanihutagaol@gmail.com>
2020-08-31 09:11:39 +0700

    set up github pages

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,13 @@
 # abort on errors
 set -e
 
-# build
 npm run build
-
-# navigate into the build output directory
 cd dist
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
 git push -f git@github.com:nanihutagaol/my-portfolio.git master:gh-pages
 
 cd -
